d234f7463fc48a254d640a85b0c6c7c2855ed2c3
Anna Makarchuk <aniki.das@gmail.com>
2019-03-21 23:00:18 +0200

    HW-1: second part - root folder and screenshots

---
+++ HomeWork_1/root/src/com/epam/courses/Hello.java
@@ -0,0 +1,13 @@
+package com.epam.courses;
+
+import javax.sound.midi.Soundbank;
+
+public class Hello {
+    public Hello() {
+        System.out.println("Hello from first class");
+    }
+
+       public static void main(String[] args) {
+           System.out.println("Hello");
+    }
+}
---
+++ HomeWork_1/root/src/com/epam/courses2/Hello2.java
@@ -0,0 +1,10 @@
+package com.epam.courses2;
+
+import com.epam.courses.Hello;
+
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("Create class Hello");
+        Hello hello = new Hello();
+    }
+}
