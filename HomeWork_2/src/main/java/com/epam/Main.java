b986262e19f39baaebdeb71a808ad4f473316dd7
Anna Makarchuk <aniki.das@gmail.com>
2019-03-29 12:31:52 +0200

    HomeWork 2

---
+++ HomeWork_2/src/main/java/com/epam/Main.java
@@ -0,0 +1,10 @@
+package main.java.com.epam;
+
+import org.junit.Assert;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Anna Makarchuk");
+        Assert.fail ("it's not my Name");
+    }
+}
